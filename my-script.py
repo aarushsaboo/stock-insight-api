0cb640a3a064e894418c5ef3179bf33fb5c5f214
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2024-07-12 11:17:46 +0530

    trying to get the routes to work

--- my-script.py
+++ my-script.py
@@ -21,7 +21,7 @@ api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
 
 DEFAULT_CSV_PATH = "https://stock-insight-api.onrender.com/constants/sp500.csv"
 USER_STOCK_CSV_PATH = "https://stock-insight-api.onrender.com/constants/user_stock_data.csv"
-# app.logger.info(f"Attempting to write to: {DEFAULT_CSV_PATH}")
+app.logger.info(f"Attempting to write to: {DEFAULT_CSV_PATH}")
 
 def get_stock_data(symbol, days=90):
     base_url = "https://www.alphavantage.co/query"
@@ -146,9 +146,10 @@ def returnSomething():
     print("This is working")
     return "This is working"
 
-# @app.route('/constants/<path:filename>')
-# def serve_csv(filename):
-#     return send_from_directory(os.path.join(BASE_DIR, 'constants'), filename)
+@app.route('/constants/<path:filename>')
+def serve_csv(filename):
+    # return send_from_directory(os.path.join(BASE_DIR, 'constants'), filename)
+    return send_from_directory('constants', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
